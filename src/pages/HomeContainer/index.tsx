82d27bed9e4bf7e93d7a91a858455f876275e6a0
yanlele <331393627@qq.com>
2019-09-03 23:44:40 +0800

    [+]添加路由跳转

--- src/pages/HomeContainer/index.tsx
+++ src/pages/HomeContainer/index.tsx
@@ -1,21 +1,45 @@
-import React, {FunctionComponent} from 'react';
+import React, {FunctionComponent, useState} from 'react';
 import {connect} from 'react-redux';
-import {Layout} from 'antd';
+import {Layout, List, Card} from 'antd';
+import {withRouter } from 'react-router-dom';
+import {History} from 'history';
 
 const {Header, Content} = Layout;
+const {Item: ListItem} = List;
 
-const HomeContainer: FunctionComponent = () => {
+interface HomeContainerProps {
+  history: History;
+}
+
+const HomeContainer: FunctionComponent<HomeContainerProps> = (props) => {
+  const {history} = props;
+  console.log(history);
+
+  const [listRouter] = useState([
+    {
+      title: '数据与选择集',
+    },
+  ]);
+
   return (
     <Layout style={{backgroundColor: '#fff'}}>
       <Header style={{background: '#fff'}}>
-        header
+        <h1>D3 </h1>
       </Header>
 
       <Content style={{margin: '20px'}}>
-        content
+        <List
+          grid={{gutter: 16, column: 4}}
+          dataSource={listRouter}
+          renderItem={item => (
+            <ListItem>
+              <Card title={item.title}>Card content</Card>
+            </ListItem>
+          )}
+        />
       </Content>
     </Layout>
   );
 };
 
-export default connect()(HomeContainer);
+export default connect()(withRouter(HomeContainer));
