d03531c28fcb4bcbfaa04a8dab7cd07b22b139f4
yanlele <331393627@qq.com>
2019-09-07 19:42:17 +0800

    [+]修改mock

--- mock.js
+++ mock.js
@@ -1,11 +1,11 @@
 const indexMock = require('./mock/index');
 const selectionAndDataComponentMock = require('./mock/selectionAndDataComponentMock');
 const scaleComponentMock = require('./mock/scaleComponentMock');
-const drawComonentMock = require('./mock/drawComonentMock');
+const drawComponentMock = require('./mock/drawComponentMock');
 
 module.exports = {
   ...indexMock,
   ...selectionAndDataComponentMock,
   ...scaleComponentMock,
-  ...drawComonentMock
+  ...drawComponentMock
 };
