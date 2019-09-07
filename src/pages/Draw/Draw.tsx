b31a5efbd4bcbb1e105c6dd77e58a96ed81dbb4a
yanlele <331393627@qq.com>
2019-09-07 17:54:43 +0800

    [+]修改主页面

--- src/pages/Draw/Draw.tsx
+++ src/pages/Draw/Draw.tsx
@@ -1,11 +1,28 @@
 import React, {FunctionComponent} from "react";
+import BasePage from "../../components/BasePage/BasePage";
+import {parse} from "querystring";
+import {Location} from "history";
+import ScaleComponent from "../../components/ScaleComponent/ScaleComponent";
 
-const Draw: FunctionComponent = () => {
+interface DrawProps {
+  location: Location;
+}
+
+const Draw: FunctionComponent<DrawProps> = (props) => {
+  const {location} = props;
+  const pathObject = parse(location.search && location.search.slice(1));
+  const { name } = pathObject;
   return (
     <div>
-      Draw
+      <BasePage
+        name={name}
+        apiPrefix="class2"
+      >
+        <ScaleComponent
+          name={name}
+        />
+      </BasePage>
     </div>
   );
 };
-
 export default Draw;
